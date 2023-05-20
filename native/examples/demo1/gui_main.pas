1a9bd611e89a2533f6fbf540e918398a62e2bc1c
Christo Crause <christo.crause@gmail.com>
2023-05-20 17:27:02 +0200

    Add LTC2309 to example.

--- native/examples/demo1/gui_main.pas
+++ native/examples/demo1/gui_main.pas
@@ -2,11 +2,13 @@ unit gui_main;
 
 {$mode objfpc}{$H+}
 
+{$define useLTC2309}
+
 interface
 
 uses
-  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, ExtCtrls, A3nalogGauge,
-  MKnob, i2c, ads1x1x, pwm_pca9685;
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, ExtCtrls, StdCtrls,
+  A3nalogGauge, MKnob, LedNumber, i2c, ads1x1x, pwm_pca9685, ltc2309;
 
 type
 
@@ -14,6 +16,9 @@ type
 
   TForm1 = class(TForm)
     A3nalogGauge1: TA3nalogGauge;
+    Label1: TLabel;
+    Label2: TLabel;
+    LEDNumber1: TLEDNumber;
     mKnob1: TmKnob;
     Timer1: TTimer;
     procedure FormShow(Sender: TObject);
@@ -21,7 +26,11 @@ type
     procedure Timer1Timer(Sender: TObject);
   private
     i2cMaster: TI2cMaster;
+    {$ifdef useLTC2309}
+    adc: TLTC2309;
+    {$else}
     adc: TADS101x;
+    {$endif}
     pwm: TPwmPca9685;
   public
 
@@ -34,6 +43,18 @@ implementation
 
 const
   errMsg = 'Error: ';
+  {$ifdef useLTC2309}
+  adcAddress = $08;
+  adcChannel = 6;
+  {$else}
+  adcAddress = $48;
+  adcChannel = 3;
+  {$endif}
+  pwmAddress = $41;
+  pwmChannel = 4;
+
+var
+  val: uint16;
 
 {$R *.lfm}
 
@@ -41,14 +62,22 @@ const
 
 procedure TForm1.Timer1Timer(Sender: TObject);
 var
-  val: uint16;
   voltage: single;
+  s: string;
 begin
   if Assigned(adc) then
   begin
-    val := adc.readChannel($48, 3);
+    val := (3*val + adc.readChannel(adcAddress, adcChannel)) div 4;
+    {$ifdef useLTC2309}
+    // Assume external 10V reference
+    voltage := val / 65535 * 10;
+    {$else}
+    // Assume internal 4.096V reference
     voltage := val / 32768 * 4.096;
+    {$endif}
     A3nalogGauge1.Position := voltage;
+    s := format('%5.2f', [voltage]);
+    LEDNumber1.Caption := s;
   end;
 end;
 
@@ -58,12 +87,18 @@ begin
 
   if i2cMaster.Initialize(i2c_1) then
   begin
-    pwm.Initialize(i2cMaster);
+    pwm.Initialize(i2cMaster, pwmAddress);
     if not pwm.setPWMFreq(1000) then
       WriteLn(errMsg, 'setPWMFreq');
+    pwm.setPWM(pwmChannel, 0, 4095);
+    mKnob1.Position := 4095;
 
+    {$ifdef useLTC2309}
+    adc := TLTC2309.Create;
+    {$else}
     adc := TADS101x.Create;
-    if not Assigned(i2cMaster) or not adc.Initialize(i2cMaster, $48) then
+    {$endif}
+    if not Assigned(i2cMaster) or not adc.Initialize(i2cMaster, adcAddress) then
     begin
       WriteLn(errMsg, 'Initialize');
       FreeAndNil(adc);
@@ -75,7 +110,7 @@ end;
 
 procedure TForm1.mKnob1Change(Sender: TObject; AValue: Longint);
 begin
-  pwm.setPWM(0, 0, AValue);
+  pwm.setPWM(pwmChannel, 0, AValue);
 end;
 
 end.
