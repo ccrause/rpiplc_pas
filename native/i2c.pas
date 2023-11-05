ac6770a04f93de07bb9a85ea75b679c9078a0970
Christo Crause <christo.crause@gmail.com>
2023-11-05 14:28:38 +0200

    Fix ReadBytes method.

--- native/i2c.pas
+++ native/i2c.pas
@@ -344,26 +344,21 @@ begin
   ioctl_data.msgs := @msg;
   ioctl_data.nmsgs := 1;
 
-  Result :=  FpIOCtl(Fi2cHandle, I2C_RDWR, @ioctl_data) >= 0;
+  Result := FpIOCtl(Fi2cHandle, I2C_RDWR, @ioctl_data) >= 0;
 end;
 
 function TI2cMaster.ReadBytes(address: byte; data: PByte; size: byte): boolean;
 var
-  msgs: array[0..1] of Ti2c_msg;
+  msg: Ti2c_msg;
   ioctl_data: Ti2c_rdwr_ioctl_data;
 begin
-  msgs[0].addr := address;
-  msgs[0].flags := 0;
-  msgs[0].len := 0;
-  msgs[0].buf := nil;
+  msg.addr := address;
+  msg.flags := I2C_M_RD;
+  msg.len := size;
+  msg.buf := data;
 
-  msgs[1].addr := address;
-  msgs[1].flags := I2C_M_RD or I2C_M_NOSTART;
-  msgs[1].len := size;
-  msgs[1].buf := @data;
-
-  ioctl_data.msgs := @msgs;
-  ioctl_data.nmsgs := 2;
+  ioctl_data.msgs := @msg;
+  ioctl_data.nmsgs := 1;
 
   if (FpIOCtl(Fi2cHandle, I2C_RDWR, @ioctl_data) < 0) then
   begin
